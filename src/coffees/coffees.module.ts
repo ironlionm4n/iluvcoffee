de0cbaddd45790c0c2d6fc220109941347957d0d
Mikail Miller <65094447+ironlionm4n@users.noreply.github.com>
2024-12-16 21:26:28 -0500

    monogo db approach

--- src/coffees/coffees.module.ts
+++ src/coffees/coffees.module.ts
@@ -1,6 +1,18 @@
 import { Module } from '@nestjs/common';
 import { CoffeesController } from './coffees.controller';
 import { CoffeesService } from './coffees.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { CofeeSchema, Coffee } from './entities/coffee.entity';
+import { EventSchema } from 'src/events/entities/event.entity/event.entity';
 
-@Module({ controllers: [CoffeesController], providers: [CoffeesService] })
+@Module({
+  imports: [
+    MongooseModule.forFeature([
+      { name: Coffee.name, schema: CofeeSchema },
+      { name: Event.name, schema: EventSchema },
+    ]),
+  ],
+  controllers: [CoffeesController],
+  providers: [CoffeesService],
+})
 export class CoffeesModule {}
--- src/coffees/entities/coffee.entity.ts
+++ src/coffees/entities/coffee.entity.ts
@@ -1,6 +1,20 @@
-export class Coffee {
-  id: number;
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document } from 'mongoose';
+
+// Final mongoose collection schema name will be 'coffees'
+@Schema()
+export class Coffee extends Document {
+  @Prop()
   name: string;
+
+  @Prop()
   brand: string;
+
+  @Prop([String])
   flavors: string[];
+
+  @Prop({ default: 0 })
+  recommendations: number;
 }
+
+export const CofeeSchema = SchemaFactory.createForClass(Coffee);
